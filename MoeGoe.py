14f25c669ab9582eb176f3f46e3c75428c200239
starmoe <96931510+Bro-Xun@users.noreply.github.com>
2022-10-13 22:34:49 +0800

    Add files via upload

--- MoeGoe.py
+++ MoeGoe.py
@@ -12,6 +12,10 @@ from mel_processing import spectrogram_torch
 
 from scipy.io.wavfile import write
 
+import yaml
+import time
+import os
+
 def ex_print(text,escape=False):
     if escape:
         print(text.encode('unicode_escape').decode())
@@ -70,13 +74,21 @@ def get_label(text,label):
         return False,text
 
 if __name__ == '__main__':
-    if '--escape' in sys.argv:
-        escape=True
-    else:
-        escape=False
+    config_file = open("config.yml",'r',encoding="utf-8")
+    config_file_data = config_file.read()
+    config_file.close()
+    config_data = yaml.load(config_file_data,Loader=yaml.FullLoader)
+
+    text_list_file = open(config_data['argument_path'],'r',encoding="utf-8")
+    text_list_data = text_list_file.read()
+    text_list_file.close()
+    text_list_data = text_list_data.replace('\n\n','\n')
+    text_list = text_list_data.split('\n')
+
+    escape=False #non-stop workflow
     
-    model = input('Path of a VITS model: ')
-    config = input('Path of a config file: ')
+    model = config_data['model_path']
+    config = config_data['config_path']
     
     hps_ms = utils.get_hparams_from_file(config)
     n_speakers = hps_ms.data.n_speakers if 'n_speakers' in hps_ms.data.keys() else 0
@@ -95,61 +107,90 @@ if __name__ == '__main__':
     
     if n_symbols!=0:
         while True:
-            choice = input('TTS or VC? (t/v):')
+            choice = config_data['method']
             if choice == 't':
-                text = input('Text to read: ')
-                if text=='[ADVANCED]':
-                    text = input('Raw text:')
-                    print('Cleaned text is:')
-                    ex_print(_clean_text(text, hps_ms.data.text_cleaners),escape)
-                    continue
-                
-                length_scale,text=get_label_value(text,'LENGTH',1,'length scale')
-                noise_scale,text=get_label_value(text,'NOISE',0.667,'noise scale')
-                noise_scale_w,text=get_label_value(text,'NOISEW',0.8,'deviation of noise')
-                cleaned,text=get_label(text,'CLEANED')
+                numb = 1
+                text = 'init'
+                lang = config_data['argument_lang']
+                for i in text_list:
+                    model_num = 1
+                    ends = i[-2::]
+                    print(ends)
+                    if not len(ends) == 2:
+                        text = lang + i + lang
+                    else:
+                        if ends[1] in ['1','2','3','4','5','6','7','8','9','0']:
+                            model_num = int(ends[1])
+                            text = lang + i[:-2:] + lang
+                        else:
+                            text = lang + i + lang
 
-                stn_tst = get_text(text, hps_ms, cleaned=cleaned)
-                
-                print_speakers(speakers,escape)
-                speaker_id = get_speaker_id('Speaker ID: ')
-                out_path = input('Path to save: ')
+                    
+                    if text=='[ADVANCED]':
+                        text = input('Raw text:')
+                        print('Cleaned text is:')
+                        ex_print(_clean_text(text, hps_ms.data.text_cleaners),escape)
+                        continue
+                    
+                    length_scale,text=get_label_value(text,'LENGTH',1,'length scale')
+                    noise_scale,text=get_label_value(text,'NOISE',0.667,'noise scale')
+                    noise_scale_w,text=get_label_value(text,'NOISEW',0.8,'deviation of noise')
+                    cleaned,text=get_label(text,'CLEANED')
 
-                with no_grad():
-                    x_tst = stn_tst.unsqueeze(0)
-                    x_tst_lengths = LongTensor([stn_tst.size(0)])
-                    sid = LongTensor([speaker_id])
-                    audio = net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale, noise_scale_w=noise_scale_w, length_scale=length_scale)[0][0,0].data.cpu().float().numpy()
-                write(out_path, hps_ms.data.sampling_rate, audio)
-                
-                print('Successfully saved!')
-                ask_if_continue()
+                    stn_tst = get_text(text, hps_ms, cleaned=cleaned)
+                    
+                    print_speakers(speakers,escape)
+                    speaker_id = model_num - 1
+                    out_path = config_data['output_path'] + str(numb) + '.wav'
+
+                    with no_grad():
+                        x_tst = stn_tst.unsqueeze(0)
+                        x_tst_lengths = LongTensor([stn_tst.size(0)])
+                        sid = LongTensor([speaker_id])
+                        audio = net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale, noise_scale_w=noise_scale_w, length_scale=length_scale)[0][0,0].data.cpu().float().numpy()
+                    write(out_path, hps_ms.data.sampling_rate, audio)
+                    
+                    print('Successfully saved!')
+                    #ask_if_continue()
+                    time.sleep(config_data['time_gap'])
+                    print('succeed writting into {}.wav'.format(numb))
+                    numb += 1
+
+                    break
                 
                     
             elif choice == 'v':
-                audio_path = input('Path of an audio file to convert:\n')
-                print_speakers(speakers)
-                audio = utils.load_audio_to_torch(audio_path, hps_ms.data.sampling_rate)
+                audio_dir = config_data['audio_dir']
+                dir_audio_dir = os.listdir(audio_dir)
+                audios = re.findall('(.*?).wav','\n'.join(audio_dir))
+                for i in audios:
+                    audio_path = audio_dir + i + '.wav'
+                    print_speakers(speakers)
+                    audio = utils.load_audio_to_torch(audio_path, hps_ms.data.sampling_rate)
 
-                originnal_id = get_speaker_id('Original speaker ID: ')
-                target_id = get_speaker_id('Target speaker ID: ')
-                out_path = input('Path to save: ')
+                    originnal_id = config_data['origin']
+                    target_id = config_data['target']
+                    out_path = config_data['output_path']
 
-                y = audio.unsqueeze(0)
+                    y = audio.unsqueeze(0)
 
-                spec = spectrogram_torch(y, hps_ms.data.filter_length,
-                    hps_ms.data.sampling_rate, hps_ms.data.hop_length, hps_ms.data.win_length,
-                    center=False)
-                spec_lengths = LongTensor([spec.size(-1)])
-                sid_src = LongTensor([originnal_id])
+                    spec = spectrogram_torch(y, hps_ms.data.filter_length,
+                        hps_ms.data.sampling_rate, hps_ms.data.hop_length, hps_ms.data.win_length,
+                        center=False)
+                    spec_lengths = LongTensor([spec.size(-1)])
+                    sid_src = LongTensor([originnal_id])
 
-                with no_grad():
-                    sid_tgt = LongTensor([target_id])
-                    audio = net_g_ms.voice_conversion(spec, spec_lengths, sid_src=sid_src, sid_tgt=sid_tgt)[0][0,0].data.cpu().float().numpy()
-                write(out_path, hps_ms.data.sampling_rate, audio)
-                        
-                print('Successfully saved!')
-                ask_if_continue()
+                    with no_grad():
+                        sid_tgt = LongTensor([target_id])
+                        audio = net_g_ms.voice_conversion(spec, spec_lengths, sid_src=sid_src, sid_tgt=sid_tgt)[0][0,0].data.cpu().float().numpy()
+                    write(out_path, hps_ms.data.sampling_rate, audio)
+                            
+                    print('Successfully saved!')
+                    time.sleep(config_data['time_gap'])
+                    print('succeed writting into {}.wav'.format(i))
+
+                    break               
+
     else:
         model = input('Path of a hubert-soft model: ')
         from hubert_model import hubert_soft
--- README.md
+++ README.md
@@ -1,50 +1,68 @@
-# Links
-- [MoeGoe_GUI](https://github.com/CjangCjengh/MoeGoe_GUI)
+# MoeGoe generating-process workflow
+
+**origined from [CjangCjengh/MoeGoe](https://github.com/CjangCjengh/MoeGoe)**
+
+## usage
+
+- prepare the argument list (if choosing `t` mode)
+- edit `config.yml`
+- run `MoeGoe.py`
+
+## to prepare argument list
+
+---
+
+*a simple example:*
+
+<big>“</big>
+
+本餐厅就是为了那些对各种美味食物已经感到厌倦了的人们 [^1]
+
+为他们提供与之身份相应的食物
+
+ [^2]
+
+欢迎光临~
+
+欢迎光临~ 2 [^3]
+
+<big>”</big>
+
+---
+
+[^1]: use a whole line to express one sentence
+[^2]: you are able to use a blank line to make the list easier to read
+[^3]: use `[blank]2(a number)` to choose the model this sentence uses (default `[blank]1`)
+
+**it must be a plain text file.**
+
+## about `config.yml`
+
+```yml
+#please make sure the number of your models before you submit
+config_path: (your full path of config file)
+model_path: (your full path of model file)
+##########
+## choose one of following options
+argument_path: (your full path of argument list)
+argument_lang: (the language you want to choose) #pay attention to supported languages of your model!!!
+##[ZH] [JA] [KO] [SA] [EN] avaliable
+#OR
+audio_dir: ###audio_directory: audio input must be in form of ms-wav(*.wav)
+origin: ###Original speaker ID
+target: ###Target speaker ID
+##########
+output_path: F:\Desktop\haochi\ #(full path)
+method: #t for text2voice while v for voice2voice
+time_gap: #time gap between each process (seconds)
+```
+
+## useful links
+
 - [Pretrained models](https://github.com/CjangCjengh/TTSModels)
 
-# How to use
-Run MoeGoe.exe
-```
-Path of a VITS model: D:\Download\243_epochs.pth
-Path of a config file: D:\Download\config.json
-INFO:root:Loaded checkpoint 'D:\Download\243_epochs.pth' (iteration 243)
-```
-## Text to speech
-```
-TTS or VC? (t/v):t
-Text to read: こんにちは。
-ID      Speaker
-0       XXXX
-1       XXXX
-2       XXXX
-Speaker ID: 0
-Path to save: demo.wav
-Successfully saved!
-```
-## Voice conversion
-```
-TTS or VC? (t/v):v
-Path of an audio file to convert:
-D:\dataset\demo.wav
-ID      Speaker
-0       XXXX
-1       XXXX
-2       XXXX
-Original speaker ID: 0
-Target speaker ID: 6
-Path to save: demo.wav
-Successfully saved!
-```
-## HuBERT-VITS
-```
-Path of a hubert-soft model: D:\Download\hubert-soft.pt
-Path of an audio file to convert:
-D:\dataset\demo.wav
-ID      Speaker
-0       XXXX
-1       XXXX
-2       XXXX
-Target speaker ID: 6
-Path to save: demo.wav
-Successfully saved!
-```
+***thanks the original developer***
+
+---
+
+<sub>feel free to pull PR or write issues</sub>
